c23c0d930c06276f01fb78abfabbaf0a4acb0fd5
ivan-victoria <45040627+ivan-victoria@users.noreply.github.com>
2019-05-19 15:00:28 +0200

    First Commit
    Added Unity Project to GitHub

---
+++ VR-VehicleProjectUnity/Assets/Scripts/LogitechSteering.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogitechSteering : MonoBehaviour
+{
+    LogitechGSDK.LogiControllerPropertiesData properties;
+
+    public float xAxis, GasInput, BreakInput, ClutchInput;
+    public int CurrentGear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(LogitechGSDK.LogiSteeringInitialize(false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
+        {
+            LogitechGSDK.DIJOYSTATE2ENGINES rec;
+            rec = LogitechGSDK.LogiGetStateUnity(0);
+
+            xAxis = rec.lX;
+        }
+        else
+        {
+            print("No steering wheel connected");
+        }
+    }
+}
