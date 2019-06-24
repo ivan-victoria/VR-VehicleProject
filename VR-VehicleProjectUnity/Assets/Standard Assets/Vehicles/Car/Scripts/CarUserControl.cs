65f311d76edfecd81f9338bf00d34ff053a8c439
ivan-victoria <45040627+ivan-victoria@users.noreply.github.com>
2019-06-24 13:08:01 +0200

    Terrain and Road

--- VR-VehicleProjectUnity/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ VR-VehicleProjectUnity/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -4,11 +4,13 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
+   
     [RequireComponent(typeof (CarController))]
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-
+        public float gas;
+        public float breakk;
 
         private void Awake()
         {
@@ -25,9 +27,15 @@ namespace UnityStandardAssets.Vehicles.Car
             float f = CrossPlatformInputManager.GetAxis("Footbreak");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Break");
-            m_Car.Move(h, v, f+1, 0);
-          //  Debug.Log("Pedal:" + v);
+
+            //if (-(f + 1) < 0) { m_Car.Move(h, v, 0, 0); }
+
+               m_Car.Move(h, v, -(f + 1), 0);
+           // m_Car.Move(h, v, v, 0);
+            //   Debug.Log("PedalG:" + v);
             Debug.Log("Pedal:" + f);
+            breakk = f;
+            gas = v;
 #else
             m_Car.Move(h, v, v, 0f);
 #endif
